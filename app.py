e4cde69dd80b96eaa48218c368ab26862ed31b57
21181 <21181@wc.school.nz>
2025-04-11 12:24:14 +1200

    finished the signup section today. Linking it to the login part. I am yet to do the actual login section, but will do that for the next session.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
 @app.route('/')
 def render_homepage():  # put application's code here
     return render_template('home.html')
@@ -13,6 +12,15 @@ def render_menu():
 @app.route('/login', methods = ['POST', 'GET'])
 def render_login_page():
     return render_template('login.html')
+
+@app.route('/signup', methods = ['POST', 'GET'])
+def render_signup_page():
+    if request.method == 'POST':
+        fname = request.form.get(user_fname).title().strip()
+        lname = request.form.get(user_lname).title().strip()
+        email = request.form.get(user_email).title().strip()
+        password = reques.form.get(user_password)
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run()
