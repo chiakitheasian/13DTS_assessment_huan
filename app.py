a59fa584b6499fe267f218d0e854319c8bb59c4d
21181 <21181@wc.school.nz>
2025-05-05 09:36:37 +1200

    requeryed my database, as I can't seem to do anything with my laptop. So next I will make it so The user can actually make the journal entries.

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
+import sqlite3
+
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+DATABASE = 'health_DB'
+def connect_database(db_file):
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Exception as e:
+        print("Database connection failed:",e)
+        return None
 @app.route('/')
 def render_homepage():  # put application's code here
     return render_template('home.html')
 
 @app.route('/menu')
 def render_menu():
+    try:
+        con = connect_database(DATABASE)
+        query = "SELECT entry_date, mood, confidence_rating FROM entries"
+        cur = con.cursor()
+        cur.execute(query)
+        entries_list = cur.fetchall()
+        con.close()
 
-    return render_template('menu.html')
+        column_names = [desc[0] for desc in cur.description]
+        entries = [dict(zip(column_names,row)) for row in entries_list]
+
+        return render_template('menu.html', entries_list=entries_list)
+    except Exception as e:
+        return f"<h1>Error</h1><p>{e}</p>"
+
 
 @app.route('/contact')
 def render_contact():
@@ -30,7 +53,7 @@ def render_signup_page():
         password2 = request.form.form.get('user_password2')
 
         if password != password2:
-            return redirect("\signup?error=passwors+do+not+match")
+            return redirect("\signup?error=passwords+do+not+match")
 
         if len(password) < 8:
             return redirect("\signup?error=password+must+be+more+than+8+characters")
